92e25a0a974db0c1b7d5db82db593e856c8a0470
7U406 <76437863+7U406@users.noreply.github.com>
2021-08-12 13:40:25 +0200

    up

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,40 +23,79 @@ class PostController extends Controller
          $c=0;
         $post=new nizar();
         $post->typepartie=$request->input("tp");
-   
-        
-        $post->count=50;
-        $post->typepartie=$request->input("tp");
         $k=$request->input("tp");
        
         #$users = DB::table('nizars')->select('id','typepartie')->get();
-        $users = DB::select('select * from nizars ');
+        $users = DB::select('select * from nizars where typepartie>count and typepartie=? ',[$k]);
         #$users = DB::select('select count from  nizars where typepartie=10');
         $n=nizar::where('typepartie','=',$k)->get();
-        
+ 
+    
 
- if(!count($n))
+ /*if(!count($n))
  {
     $post->typepartie=$request->input("tp");  
     $post->count=55;
     $post->save();
  }
-  foreach($n as $value){
-           if( $value['typepartie']>$value['count'] )
+ */
+ /*for($i=0;$i<count($n);$i++)
+ {
+if($n[$i]['typepartie']==$n[$i]['count'])
+{
+ 
+    unset($n[$i]);
+}
+
+
+ }
+ */
+while($users['typepartie']>$users['count'])
+           
            {
-             $value['count']+=1;
-             $value->save();
+             $users->count=$users->count+1;
+             $post->save();
+              echo 'dekhel'; 
                 echo "nizar";        
-             echo $value['count'];
-             return view('welcome');
+                   echo $users->count;
+                 return view('welcome');
            }
-           else
+        
            {
+
             $post->typepartie=$request->input("tp");  
             $post->count=55;
             $post->save();
            
+            echo $post->count;
+            
+            return view('welcome');
+           
+          
+           }
+        /* else  if(  $value['typepartie']==$value['count'])
+           {  echo "nizar";        
             echo $value['count'];
+               echo "eq";
+             return view('welcome');
+           }
+           /*else{
+            echo "nizar";    
+            echo $value['count'];
+            return view('welcome');
+           }
+          
+         
+          
+       
+           else
+           {
+
+            $post->typepartie=$request->input("tp");  
+            $post->count=55;
+            $post->save();
+           
+            echo $post->count;
             
             return view('welcome');
            
