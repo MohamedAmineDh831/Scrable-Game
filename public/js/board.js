4b9d3f8fb8e0d54fc76ce0126084fb9eb74c43c5
Dh.MohamedAmine <82577666+MohamedDh1919@users.noreply.github.com>
2021-08-14 07:54:29 +0100

    Calcul de Score

--- public/js/board.js
+++ public/js/board.js
@@ -1,25 +1,30 @@
 
 
+var grille = [[4,0,0,1,0,0,0,4,0,0,0,1,0,0,4],
+              [0,3,0,0,0,2,0,0,0,2,0,0,0,3,0],
+              [0,0,3,0,0,0,1,0,1,0,0,0,3,0,0],
+              [1,0,0,3,0,0,0,1,0,0,0,3,0,0,1],
+              [0,0,0,0,3,0,0,0,0,0,3,0,0,0,0],
+              [0,2,0,0,0,2,0,0,0,2,0,0,0,2,0],
+              [0,0,1,0,0,0,1,0,1,0,0,0,1,0,0],
+              [4,0,0,1,0,0,0,5,0,0,0,1,0,0,4],
+              [0,0,1,0,0,0,1,0,1,0,0,0,1,0,0],
+              [0,2,0,0,0,2,0,0,0,2,0,0,0,2,0],
+              [0,0,0,0,3,0,0,0,0,0,3,0,0,0,0],
+              [1,0,0,3,0,0,0,1,0,0,0,3,0,0,1],
+              [0,0,3,0,0,0,1,0,1,0,0,0,3,0,0],
+              [0,3,0,0,0,2,0,0,0,2,0,0,0,3,0],
+              [4,0,0,1,0,0,0,4,0,0,0,1,0,0,4]
+
+]
+
 function initialiser_grille(){
-    var arrayLignes = document.getElementById("tb").rows; //on récupère les lignes du tableau
-    var longueur = arrayLignes.length;//on peut donc appliquer la propriété length
 
-
-    for(var i=0; i<longueur; i++)//on peut directement définir la variable i dans la boucle
-    {
-        var arrayColonnes = arrayLignes[i].cells;//on récupère les cellules de la ligne
-        var largeur = arrayColonnes.length;
-
-        for(var j=0; j<largeur; j++)
-        {
-            if((i<3)||((i==3)&&(j==0))){
-                arrayColonnes[j].innerText = 'A';
-            }
-            else {
-                arrayColonnes[j].childNodes[0].removeChild(arrayColonnes[j].childNodes[0].childNodes[0]);
-                arrayColonnes[j].childNodes[0].innerText = 'A';
-                //console.log(arrayColonnes[j].childNodes[0]);
-            }
+    for( var j=0; j<15; j++) {
+        for (var i = 0; i < 15; i++) {
+            id = "BoardSquareOrTile_" +i+ "x" +j;
+            //console.log(id);
+            $('#' + id)[0].innerText = 'A';
         }
     }
 }
@@ -29,15 +34,50 @@ function integ_mot(mot_obj){
     var id ="BoardSquareOrTile_"+mot_obj.ligne+"x"+mot_obj.colonne;
     if(mot_obj.direction === 'h'){
         for(var i=0;i<mot_obj.mot.length;i++){
-            id ="BoardSquareOrTile_"+(mot_obj.colonne+i)+"x"+mot_obj.ligne;
+            id ="BoardSquareOrTile_"+(mot_obj.colonne+i-1)+"x"+(mot_obj.ligne-1);
             $('#'+id)[0].innerText = mot_obj.mot[i];
         }
     }
     else {
         for(var i=0;i<mot_obj.mot.length;i++){
-            id ="BoardSquareOrTile_"+mot_obj.colonne+"x"+(mot_obj.ligne+i);
+            id ="BoardSquareOrTile_"+(mot_obj.colonne-1)+"x"+(mot_obj.ligne+i-1);
             $('#'+id)[0].innerText = mot_obj.mot[i];
         }
     }
 
 }
+
+function calcul_score(mot_obj){
+    console.log(grille);
+    var lettre = 0;
+    var score = 0;
+    var multiple_score = 1;
+    for (let i=0; i<mot_obj.mot.length; i++){
+        lettre = (mot_obj.mot[i].toUpperCase()).charCodeAt(0)-65;
+        console.log(reserve[lettre]);
+        score += reserve[lettre].Points;
+        var x = mot_obj.ligne-1;
+        var y = mot_obj.colonne-1;
+        if(mot_obj.direction === 'h'){
+            if(grille[x][y+i] === 1){
+                score += reserve[lettre].Points;
+                grille[x][y+i] = 0;
+            }
+            else if(grille[x][y+i] === 2){
+                score += 2*(reserve[lettre].Points);
+                grille[x][y+i] = 0;
+            }
+            else if(grille[x][y+i] === 3){
+                multiple_score = 2;
+                grille[x][y+i] = 0;
+            }
+            else if(grille[x][y+i] === 4){
+                multiple_score = 3;
+                grille[x][y+i] = 0;
+            }
+        }
+    }
+
+    return score*multiple_score;
+}
+
---
+++ public/js/reserve.js
@@ -0,0 +1,30 @@
+
+
+var reserve = [
+    {'Lettre': 'A', 'Quantity': 9, 'Points': 1},
+    {'Lettre': 'B', 'Quantity': 2, 'Points': 3},
+    {'Lettre': 'C', 'Quantity': 2, 'Points': 3},
+    {'Lettre': 'D', 'Quantity': 3, 'Points': 2},
+    {'Lettre': 'E', 'Quantity': 15, 'Points': 1},
+    {'Lettre': 'F', 'Quantity': 2, 'Points': 4},
+    {'Lettre': 'G', 'Quantity': 2, 'Points': 2},
+    {'Lettre': 'H', 'Quantity': 2, 'Points': 4},
+    {'Lettre': 'I', 'Quantity': 8, 'Points': 1},
+    {'Lettre': 'J', 'Quantity': 1, 'Points': 8},
+    {'Lettre': 'K', 'Quantity': 1, 'Points': 10},
+    {'Lettre': 'L', 'Quantity': 5, 'Points': 1},
+    {'Lettre': 'M', 'Quantity': 3, 'Points': 2},
+    {'Lettre': 'N', 'Quantity': 6, 'Points': 1},
+    {'Lettre': 'O', 'Quantity': 6, 'Points': 1},
+    {'Lettre': 'P', 'Quantity': 2, 'Points': 3},
+    {'Lettre': 'Q', 'Quantity': 1, 'Points': 8},
+    {'Lettre': 'R', 'Quantity': 6, 'Points': 1},
+    {'Lettre': 'S', 'Quantity': 6, 'Points': 1},
+    {'Lettre': 'T', 'Quantity': 6, 'Points': 1},
+    {'Lettre': 'U', 'Quantity': 6, 'Points': 1},
+    {'Lettre': 'V', 'Quantity': 2, 'Points': 4},
+    {'Lettre': 'W', 'Quantity': 1, 'Points': 10},
+    {'Lettre': 'X', 'Quantity': 1, 'Points': 10},
+    {'Lettre': 'Y', 'Quantity': 1, 'Points': 10},
+    {'Lettre': 'Z', 'Quantity': 1, 'Points': 10}
+]
