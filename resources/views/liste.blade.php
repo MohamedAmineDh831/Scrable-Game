80866e1297c47480de62d0deb1c3cd1fd624cb8a
7U406 <76437863+7U406@users.noreply.github.com>
2021-08-16 10:13:57 +0200

    update

--- resources/views/liste.blade.php
+++ resources/views/liste.blade.php
@@ -1,5 +1,12 @@
 <h3>en cours d'attente</h3>
 <h3>{{$o}}</h3>
+
+<script type="text/javascript">
+        setTimeout(function(){
+            location.reload();
+        },15000);
+    </script>
+
 @if($o==61)
 <input type="submit" value="Submit">
 @endif
