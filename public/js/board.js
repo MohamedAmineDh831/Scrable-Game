c3eee56b9a230ee66f4e7f09ca0403085e2220e2
Dh.MohamedAmine <82577666+MohamedDh1919@users.noreply.github.com>
2021-08-17 17:47:42 +0100

    Verification de l'integration d'un mot au board
    verifier tous les conditions nécéssaires pour intégrer um mot dans le Board etpréciser les lesttres demmandés de chevalet

--- public/js/board.js
+++ public/js/board.js
@@ -53,12 +53,70 @@ function update_board(mot_obj){
         var x = mot_obj.ligne-1;
         var y = mot_obj.colonne-1;
         if(mot_obj.direction === 'h'){
-            grille[x][y+i] = 0;
+            grille[x][y+i] = -1;
         }
         else{
-            grille[x+i][y] = 0;
+            grille[x+i][y] = -1;
         }
     }
 }
+
+function verif_board(mot_obj){
+    var verif = true;
+    var ch = '';
+    try {
+        if (((mot_obj.direction === 'h') && (mot_obj.colonne + mot_obj.mot.length > 16))||((mot_obj.direction === 'v') && (mot_obj.ligne + mot_obj.mot.length > 16)))
+        {
+            throw SyntaxError('ligne insufisant our intégrer cette mot');
+        }
+        for (let i=0; i<mot_obj.mot.length; i++){
+            var x = mot_obj.ligne-1;
+            var y = mot_obj.colonne-1;
+            var id ="BoardSquareOrTile_"+(y+i)+"x"+x;
+            if(mot_obj.direction === 'h'){
+                id = "BoardSquareOrTile_" + (y + i) + "x" + x;
+                if ((grille[x][y + i] === -1) && ($('#' + id)[0].innerText !== mot_obj.mot[i])) {
+                    throw SyntaxError('emplacement incorrecte');
+                }
+                else if(grille[x][y+i] !== -1){
+                    ch +=mot_obj.mot[i];
+                }
+            }
+            else{
+                id ="BoardSquareOrTile_"+(mot_obj.colonne-1)+"x"+(mot_obj.ligne+i-1);
+                if((grille[x+i][y] === -1)&&($('#'+id)[0].innerText !== mot_obj.mot[i])){
+                     throw SyntaxError('emplacement incorrecte');
+                }
+                else if(grille[x+i][y] !== -1){
+                    ch +=mot_obj.mot[i];
+                }
+            }
+        }
+        console.log(ch);
+        if(ch.length === mot_obj.mot.length){
+            throw SyntaxError('mot nest pas liée');
+        }
+    }
+    catch(e){
+        console.log(e.message);
+           verif = false;
+    }
+    finally {
+        return {'verif': verif, 'chev': ch};
+    }
+}
+
+
+
+
+/*
+     ### le test de la fonction verif est le code suivant :
+        initialiser_grille();
+        integ_mot({'mot': 'motus', 'ligne': 1, 'colonne': 11, 'direction': 'h'});
+        update_board({'mot': 'motus', 'ligne': 1, 'colonne': 11, 'direction': 'h'});
+        console.log(verif_board({'mot': 'atu', 'ligne': 1, 'colonne': 12, 'direction': 'h'}));
+     ### on jue avec les valeurs des parametres mot_obj pour tester cette fonction
+ */
+
 
 
